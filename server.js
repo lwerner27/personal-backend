96e1aaff00ed702c3151450ad62e0c876aac8ea5
Lucas Werner <lwerner27@gmail.com>
2019-02-06 14:14:07 -0600

    setup server with external router and mongodb connection

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require('body-parser');
+const routes = require('./routes');
 
+const PORT = process.env.PORT || 8080
 const app = express()
 
-const port = process.env.PORT || 5300
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(routes)
 
-app.listen(port, () => {
-    console.log("Server listening on port: " + port)
+// Connect to the Mongo DB
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/TestAuth', {
+        useNewUrlParser: true
+    }
+);
+
+
+app.listen(PORT, () => {
+    console.log("The server is listening on port " + 8080)
 })
