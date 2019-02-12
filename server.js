54ab3553c3613d3b4bca01fd70fd8c3b412756fd
Lucas Werner <lwerner27@gmail.com>
2019-02-12 12:08:37 -0600

    changed backup port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(bodyParser.urlencoded({
