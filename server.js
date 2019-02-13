a6b4b27d3b92057ea342ba120c37c07e3b1546ce
Lucas Werner <lwerner27@gmail.com>
2019-02-13 09:09:46 -0600

    attempting cors configuration

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const mongoose = require("mongoose")
 const bodyParser = require('body-parser');
 const routes = require('./routes');
@@ -6,11 +7,27 @@ const routes = require('./routes');
 const PORT = process.env.PORT || 5000
 const app = express()
 
+// const corsOptions = {
+//     origin: '*',
+//     optionsSuccessStatus: 200
+// }
+// app.use(cors(corsOptions))
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 app.use(routes)
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    if (req.method === 'OPTIONS') {
+        res.header('Acces-Control-Allow-Methods', "PUT, POST, PATCH, GET, DELETE")
+        return res.status(200).json({})
+    }
+    next()
+})
 
 // Connect to the Mongo DB
 mongoose.connect(
